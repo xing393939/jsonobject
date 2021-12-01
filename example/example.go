79779df6e163dcb3f80469e9946583bc96b1c1e9
chenxing <chenxing@bookan.com.cn>
2021-12-01 22:23:40 +0800

    edit README.md

--- example/example.go
+++ example/example.go
@@ -3,13 +3,24 @@ package main
 import "github.com/xing393939/jsonobject"
 
 func main() {
-	jsonContent := `{"name":"John", "score":100}`
+	jsonContent := `{
+		"isMaster": false, 
+		"metadata": {
+			"name": "oracle"
+		},
+		"tags": ["db", "sql"]
+	}`
 	jo := jsonobject.NewJsonObject(jsonContent)
-	println(jo.GetString("name"), jo.GetInt("score"))
+	println(jo.GetBool("isMaster"))
+	println(jo.GetJsonObject("metadata").GetString("name"))
+	joArr := jo.GetJsonObjectSlice("tags")
+	for _, joRow := range joArr {
+		println(joRow.GetString())
+	}
 
 	jsonContent = `[{"name":"John", "score":100}, {"name":"Tom", "score":200}]`
 	jo = jsonobject.NewJsonObject(jsonContent)
-	joArr := jo.GetJsonObjectSlice()
+	joArr = jo.GetJsonObjectSlice()
 	for _, joRow := range joArr {
 		println(joRow.GetString("name"), joRow.GetInt("score"))
 	}
